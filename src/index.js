5b53c8fad19c2f4b96b60526f1e1bc1847ccecac
marmooo <marmooo@users.noreply.github.com>
2024-05-28 13:20:27 +0900

    improve audio loading

--- src/index.js
+++ src/index.js
@@ -14,10 +14,8 @@ const problems = {};
 let correctCount = 0;
 let englishVoices = [];
 loadVoices();
-const audioContext = new globalThis.AudioContext();
+let audioContext;
 const audioBufferCache = {};
-loadAudio("end", "/emoji-clicker/mp3/end.mp3");
-loadAudio("correct", "/emoji-clicker/mp3/correct3.mp3");
 loadConfig();
 
 function loadConfig() {
@@ -73,33 +71,56 @@ function getTTSLang() {
   }
 }
 
-async function playAudio(name, volume) {
-  const audioBuffer = await loadAudio(name, audioBufferCache[name]);
-  const sourceNode = audioContext.createBufferSource();
-  sourceNode.buffer = audioBuffer;
-  if (volume) {
-    const gainNode = audioContext.createGain();
-    gainNode.gain.value = volume;
-    gainNode.connect(audioContext.destination);
-    sourceNode.connect(gainNode);
-    sourceNode.start();
+function createAudioContext() {
+  if (globalThis.AudioContext) {
+    return new globalThis.AudioContext();
   } else {
-    sourceNode.connect(audioContext.destination);
-    sourceNode.start();
+    console.error("Web Audio API is not supported in this browser");
+    return null;
   }
 }
 
-async function loadAudio(name, url) {
-  if (audioBufferCache[name]) return audioBufferCache[name];
-  const response = await fetch(url);
-  const arrayBuffer = await response.arrayBuffer();
-  const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
-  audioBufferCache[name] = audioBuffer;
-  return audioBuffer;
+function unlockAudio() {
+  if (audioContext) {
+    audioContext.resume();
+  } else {
+    audioContext = createAudioContext();
+    loadAudio("end", "/emoji-clicker/mp3/end.mp3");
+    loadAudio("correct", "/emoji-clicker/mp3/correct3.mp3");
+  }
+  document.removeEventListener("pointerdown", unlockAudio);
+  document.removeEventListener("keydown", unlockAudio);
 }
 
-function unlockAudio() {
-  audioContext.resume();
+async function loadAudio(name, url) {
+  if (!audioContext) return;
+  if (audioBufferCache[name]) return audioBufferCache[name];
+  try {
+    const response = await fetch(url);
+    const arrayBuffer = await response.arrayBuffer();
+    const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+    audioBufferCache[name] = audioBuffer;
+    return audioBuffer;
+  } catch (error) {
+    console.error(`Loading audio ${name} error:`, error);
+    throw error;
+  }
+}
+
+function playAudio(name, volume) {
+  if (!audioContext) return;
+  const audioBuffer = audioBufferCache[name];
+  if (!audioBuffer) {
+    console.error(`Audio ${name} is not found in cache`);
+    return;
+  }
+  const sourceNode = audioContext.createBufferSource();
+  sourceNode.buffer = audioBuffer;
+  const gainNode = audioContext.createGain();
+  if (volume) gainNode.gain.value = volume;
+  gainNode.connect(audioContext.destination);
+  sourceNode.connect(gainNode);
+  sourceNode.start();
 }
 
 function loadVoices() {
@@ -411,7 +432,5 @@ document.getElementById("voice").onclick = () => {
   speak(answer);
 };
 document.getElementById("lang").onchange = changeLang;
-document.addEventListener("click", unlockAudio, {
-  once: true,
-  useCapture: true,
-});
+document.addEventListener("pointerdown", unlockAudio, { once: true });
+document.addEventListener("keydown", unlockAudio, { once: true });
--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "2024-03-11 09:50";
+const CACHE_NAME = "2024-05-28 13:20";
 const urlsToCache = [
   "/emoji-clicker/",
   "/emoji-clicker/index.js",
